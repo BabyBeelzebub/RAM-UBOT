3b9be63c0c1867bd8bc9568d5f8a37c67e8d7b3a
•ᴄʜєᴧ✞̸.ɛxɛ✰̶✰̶ <89534912+BabyBeelzebub@users.noreply.github.com>
2021-11-24 15:03:32 +0700

    Create War.py

---
+++ userbot/modules/War.py
@@ -0,0 +1,274 @@
+# Create by myself @localheart
+# Peyy Minta ya
+
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.ywc(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Sama sama kawan**")
+
+
+@register(outgoing=True, pattern=r"^\.jamet(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**WOII**")
+    sleep(1.5)
+    await typew.edit("**JAMET**")
+    sleep(1.5)
+    await typew.edit("**CUMA MAU BILANG**")
+    sleep(1.5)
+    await typew.edit("**GAUSAH SO ASIK**")
+    sleep(1.5)
+    await typew.edit("**EMANG KENAL?**")
+    sleep(1.5)
+    await typew.edit("**GAUSAH REPLY**")
+    sleep(1.5)
+    await typew.edit("**GA USA SOKAB**")
+    sleep(1.5)
+    await typew.edit("**GASUKA PC ANJING**")
+    sleep(1.5)
+    await typew.edit("**BOCAH KAMPUNG**")
+    sleep(1.5)
+    await typew.edit("**MENTAL TEMPE**")
+    sleep(1.5)
+    await typew.edit("**LEMBEK NGENTOT🔥**")
+
+
+@register(outgoing=True, pattern=r"^\.asu(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**LU TAU ANJING GA? IYA ITU LU 1 SPESIES TAU GAA KNP DARI TADI LU GONGGONG BACOT DOANG ISINYA NGEHINA PARAH BET SI HINA BET GA ZII**")
+
+
+@register(outgoing=True, pattern=r"^\.cel(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**EH BABI HUTAN ITEM BURIK DEKIL KALO LU CMN BISA TYPING TANPA MATERI SAMA AJA KAYA CELENG CMN BISA MAJU MUNDUR PAHAM GA BEGO?**")
+
+
+@register(outgoing=True, pattern=r"^\.bljr(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**BANYAKIN BELAJAR TOLOL BIAR PINTER NYARI KOSA KATA MAKAN NYE KALO SEKOLAH YANG BENER DONGO EH IYE GUA LUPA LU ANAK YATIM MUKA MUKA MACAM LU MANA BISA SEKOLAH MALU ORANG TUA LU AJA KAGA JELAS SIAPA TOLOL**")
+
+
+@register(outgoing=True, pattern=r"^\.pp(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**PASANG PP DULU GOBLOK,BIAR ORANG-ORANG PADA TAU BETAPA HINA NYA MUKA LU 😆**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.ds(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**BAGI BOCAH BOCAH TELE KARBITAN YG BARU TERBIT KALO LU GA TAU DOUBLE STAR? INGAT DI ATAS LANGIT MASIH ADA BINTANG**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.kurus(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**BOCAH MISKIN KEKURANGAN GIZI LU AJA UDAH KURUS KAYAK KANGKUNG GITU ANJING SINI GW KASI PAHAM SAMA LU PILIH DAN BELI MAKAN MAKANAN YANG BERNUTRISI BIAR OTAK LU CERDAS BISA NYUSUN KOSA KATA YG BERDAMAGE DIKITAN**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.krs(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**OWH IYA GW BARU SADAR KALO KELUARGA LU FAKIR MISKIN YANG BUTUH BANTUAN DONASI BTW INI GW JG BANTU NYALURIN BANSOS LU MAU GA? SINI GW BAGI BIAR BISA MAKAN NYAMBUNG KEHIDUPAN TRUS MERATAPI NASIB YANG NGEBANGKEDI KOLONG JEMBATAN**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.radio(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**PAHAM SEBERAPA MISKIN KELUARGA LO? LO ITU ANAK YG LAHIRNYA NGUTANG DIRUMAH SAKIT GOBLOK KELUARGA LO SANGKING MISKINNYA NGUTANG DIRUMAH SAKIT PAS NGELAHIRIN LO TOLOL, DAN LO YG DIAZANIN LEWAT RADIO LAMA DONGO. **"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.tata(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**SUSUNAN KATA MACEM APA INI? ARTIKULASI GA JELAS TOLOL YG BIKIN! HAHA MIRIS GUE LIATNYA NGENTOT, MENDING LO BANTUIN EMAK LO JADI NELAYAN DEH GAGUNA LO MAEN TELE, SONO BANTUIN BEGO RELA PANAS²AN DIBAWAH TERIK MATAHARI LO MALAH DITELE GOBLOK, EMG ANAK GATAU DIRI LO. **"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.haram(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**EH ANAK HARAM GW KASI PAHAM! EMAKLO TUH AIB KELUARGA DIA DIBULLY SAMA TETANGA DAN SEKSRANG HIDUPNYA NGELUH MAU MATI AJA KATANYA YAIYALAH JABLAH MENDING MATI AJA DARI PADA GAGUNA HIDUPNYA LONTE MLU UDH KENA HIV MLU GOBLOK!DAN SEKARANG MAKLO MENGIDAM PENYAKIT KANGKER RAHIM GIH LO BURU BURU NYARI DANA DAN BIAYA RUMAH SAKITNYA GOBLOK RAMBUTNYA MULAI RONTOK RONTOK PERLAHAN LAHAN ANJING GAKASIAN SAMA IBULO YG JABLAY NASIBNYA MALANG MELINTANG GITU. **"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.bapa(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**EH GOBLOK BAPALO TUH PENGEMIS TAPI SUKA BGT MABOK MABOKAN ANJING UDB MISKIN HIDUPNYA JADI PENGEMIS SENGSARA MLU EH MALAH MASIH DOYAN JUDI SAMA MABOK AJA ANJING PANTES AJA HIDUPLO GAPERNAH BERKAH GOBLOK ORG LO DINAFKAHIN DUIT HASIL JUDI SAMA HASIL NGEMIS PAS BGT ITU DOSANYA SEIMBANG ANJING, HARAM DAN HALAL CEWE HASIL ANAL DAN DILDO AHAHAHAH CUIH  **"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.dp(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MUKA LU HINA, GAUSAH SOK KERAS YA ANJENGG!!**")
+
+
+@register(outgoing=True, pattern=r"^\.dp(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MUKA LU HINA, GAUSAH SOK KERAS YA ANJENGG!!**")
+
+
+@register(outgoing=True, pattern=r"^\.so(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GAUSAH SOKAB SAMA GUA GOBLOK, LU BABU GA LEVEL!!**")
+
+
+@register(outgoing=True, pattern=r"^\.nb(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MAEN BOT MULU ALAY NGENTOTT, KESANNYA NORAK GOBLOK!!!**")
+
+
+@register(outgoing=True, pattern=r"^\.met(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**NAMANYA JUGA JAMET CAPER SANA SINI BUAT CARI NAMA**")
+
+
+@register(outgoing=True, pattern=r"^\.but(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**BOT BOT APA SI PAKE BOT SURREND PALA BAPAK KAU SURREND GINI GW KASI PAHAM BUAT MANUSIA BEGO KAYA LU YG GA BISA PAKE BOT SOSOAN LAMA MAIN TELE BOCAH LAMA TAPI GA BISA MANFAATIN CUMAN BISA WAR WAR TAI ANJING WAHAI SAMPAH GA ADA MANFAATNYA KASTA RENDAH USER TELEGRAM**"
+                     )
+
+
+@register(outgoing=True, pattern=r"^\.war(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**WAR WAR PALAK BAPAK KAU WAR, SOK KERAS BANGET GOBLOK, DI TONGKRONGAN JADI BABU, DI TELE SOK JAGOAN...**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.wartai(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**WAR WAR TAI ANJING, KETRIGGER MINTA SHARELOK LU KIRA MAU COD-AN GOBLOK, KETIKAN LU AJA KGA ADA KERAS KERASNYA GOBLOK**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.kismin(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**CUIHHHH, MAKAN AJA MASIH NGEMIS LO GOBLOK, JANGAN SO NINGGI DEPAN GW YA KONTOL GA KEREN LU KEK GITU DONGO!!**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.ded(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**MATI AJA LU GOBLOK, GAGUNA LU HIDUP DI BUMI MERUSAK PEMANDANGAN**")
+
+
+@register(outgoing=True, pattern=r"^\.sokab(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**SOKAB BET LU GOBLOK, KAGA ADA ISTILAH NYA BAWAHAN TEMENAN AMA BOS!!**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.gembel(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**MUKA BAPAK LU KEK KELAPA SAWIT ANJING, GA USAH NGATAIN ORANG, MUKA LU AJA KEK GEMBEL TEXAS GOBLOK!!**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.cuih(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**GAK KEREN LO KEK BEGITU GOBLOK, KELUARGA LU BAWA SINI GUA LUDAHIN SATU-SATU. CUIHH!!!**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.dih(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**DIHHH NAJISS ANAK HARAM LO GOBLOK, JANGAN BELAGU DIMARI KAGA KEREN LU KEK BGITU TOLOL!**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.gcs(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**GC SAMPAH KAYA GINI, BUBARIN AJA GOBLOK!!**")
+
+
+@register(outgoing=True, pattern=r"^\.skb(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**EMANG KITA KENAL? KAGA GOBLOK!!! SOKAB BANGET LU DUNGU!!**")
+
+
+CMD_HELP.update(
+    {
+        "war": "**Plugin : **`war`\
+        \n\n  •  **Syntax :** `.jamet`\
+        \n  •  **Function : **Menghina Jamet telegram\
+        \n\n  •  **Syntax :** `.pp`\
+        \n  •  **Function : **Menghina Jamet telegram yang ga pake foto profil\
+        \n\n  •  **Syntax :** `.dp`\
+        \n  •  **Function : **Menghina Jamet muka hina!\
+        \n\n  •  **Syntax :** `.so`\
+        \n  •  **Function : **Ngeledek orang sokab\
+        \n\n  •  **Syntax :** `.nb`\
+        \n  •  **Function : **Ngeledek orang norak baru pake bot\
+        \n\n  •  **Syntax :** `.so`\
+        \n  •  **Function : **Ngeledek orang sokab\
+        \n\n  •  **Syntax :** `.skb`\
+        \n  •  **Function : **Ngeledek orang sokab versi 2\
+        \n\n  •  **Syntax :** `.met`\
+        \n  •  **Function : **Ngeledek si jamet caper\
+        \n\n  •  **Syntax :** `.war`\
+        \n  •  **Function : **Ngeledek orang so keras ngajak war\
+        \n\n  •  **Syntax :** `.wartai`\
+        \n  •  **Function : **Ngeledek orang so ketrigger ngajak cod minta sharelok\
+        \n\n  •  **Syntax :** `.kismin`\
+        \n  •  **Function : **Ngeledek orang kismin so jagoan di tele\
+        \n\n  •  **Syntax :** `.ded`\
+        \n  •  **Function : **Nyuruh orang mati aja goblok wkwk\
+        \n\n  •  **Syntax :** `.sokab`\
+        \n  •  **Function : **Ngeledek orang so kenal so dekat padahal kga kenal goblok\
+        \n\n  •  **Syntax :** `.gembel`\
+        \n  •  **Function : **Ngeledek bapaknya si jamet\
+        \n\n  •  **Syntax :** `.cuih`\
+        \n  •  **Function : **Ngeludahin keluarganya satu satu wkwk\
+        \n\n  •  **Syntax :** `.dih`\
+        \n  •  **Function : **Ngeledek anak haram\
+        \n\n  •  **Syntax :** `.gcs`\
+        \n  •  **Function : **Ngeledek gc sampah\
+        \n\n**Klo mau Req, kosa kata dari lu Hubungi @Untelee**\
+    "
+    }
+)
